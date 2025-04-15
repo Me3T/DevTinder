021c60e5b51d8751dde4bcf0f3c88350978877a3
Me3T <rai.manmeet1998@gmail.com>
2025-04-15 14:25:43 +0530

    Moved Mongo URI to .env

--- src/app.js
+++ src/app.js
@@ -1,19 +1,32 @@
 const express = require("express");
+const connectDB = require("./config/database");
+const User = require("./models/user");
 
 const app = express();
 
-app.get("/user", (req, res) => {
-  res.send({ name: "Manmeet Rai" });
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Manmeet",
+    lastName: "Rai",
+    email: "m@r.com",
+    password: "manmeet@123",
+  });
+
+  try {
+    await user.save();
+    res.send("User Added");
+  } catch (error) {
+    res.status(400).send("Error saving the user");
+  }
 });
 
-app.post("/user", (req, res) => {
-  res.send("Hello from user");
-});
-
-app.use("/test", (req, res) => {
-  res.send("Hello from test");
-});
-
-app.listen(7777, () => {
-  console.log("Server is Running");
-});
+connectDB()
+  .then(() => {
+    console.log("Connection Established");
+    app.listen(7777, () => {
+      console.log("Server is Running");
+    });
+  })
+  .catch((err) => {
+    console.log("connection not established");
+  });
