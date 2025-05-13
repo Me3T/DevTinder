b4511e7883d243899cf14b76fd0e33d5777eab80
Me3T <rai.manmeet1998@gmail.com>
2025-05-13 16:02:14 +0530

    profile update api

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
+const { validateEditProfileData } = require("../utils/validation");
 
 const profileRouter = express.Router();
 
@@ -10,6 +11,24 @@ profileRouter.get("/profile/view", userAuth, async (req, res) => {
     res.send(user);
   } catch (error) {
     res.status(400).send("Error Logging In" + error.message);
+  }
+});
+
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    if (!validateEditProfileData) {
+      throw new Error("Invalid Edit Request");
+    }
+
+    const loggedInUser = req.user;
+
+    Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
+
+    await loggedInUser.save();
+
+    res.send("Profile updated successfully");
+  } catch (err) {
+    res.status(400).send("Error: " + err.message);
   }
 });
 
