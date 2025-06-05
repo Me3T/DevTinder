685519567316652f6360ff953d34e874d8051ae0
Me3T <rai.manmeet1998@gmail.com>
2025-06-05 19:17:28 +0530

    schema update

--- src/models/user.js
+++ src/models/user.js
@@ -8,7 +8,7 @@ const userSchema = new mongoose.Schema(
     firstName: {
       type: String,
       required: true,
-      minLength: 2,
+      minLength: 4,
       maxLength: 50,
     },
     lastName: {
