6b2dfcfd60868aa4a48366f421a00b417195c0eb
Me3T <rai.manmeet1998@gmail.com>
2025-06-18 17:44:12 +0530

    validation

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -68,7 +68,7 @@ authRouter.post("/logout", async (req, res) => {
   res.cookie("token", null, {
     expires: new Date(Date.now()),
   });
-  res.send("Logout Successfull");
+  res.send("You have been logged out successfully");
 });
 
 module.exports = authRouter;
